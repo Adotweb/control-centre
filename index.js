0869784fbb43340c794d18eb754e725002de8aa7
Alim Weber <alim.weber@outlook.com>
2025-02-05 17:53:02 +0100

    add typing + rightclick

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const cors = require("cors");
 const { exec } = require("child_process")
 
 
+app.use(express.json())
+
 app.use(cors())
 
 app.post("/keyboard", (req, res) => {
 
 	let { key, modifier } = req.body;
 
-	modifier = modifier ? "${modifier}+" :"";
+	modifier = modifier ? `${modifier}+` :"";
 
 
 	key = key.toLowerCase();
@@ -31,24 +33,53 @@ app.post("/keyboard", (req, res) => {
 		')' : 9,
 		'=' : 0,
 
-		'{' : 7,
-		'[' : 8,
-		']' : 9,
-		'}' : 0,
 
 		"escape" : "Escape",
 		"tab" : "Tab",
-		"capslock" : "Escape"
+		"capslock" : "Escape",
+		" " : "space",
+		"backspace" : "BackSpace"
 	}
+
+	let type_map = [
+		"=", 
+		":",
+		";",
+		",",
+		".",
+		"-",
+		"_",
+		"?",
+		"´",
+		"`",
+		"^",
+		"°",
+		"|",
+		"<",
+		">",
+		"\\",
+		"{",
+		"[",
+		"]",
+		"}"
+	] 
 
 
 	if(key_map[key]){
 		key = key_map[key]
 	}
+	if(type_map.includes(key)){
+		exec(`xdotool type ${key}`)
+		return
+	}
 
 	modifier = modifier.toLowerCase();
 
-	exec(`xdotool key ${modifier}${key}`);
+	let r = (`xdotool key ${modifier}${key}`);
+
+	console.log(r)
+
+	exec(r)
 
 	res.send("")
 
@@ -59,9 +90,12 @@ app.post("/mouse", (req, res) => {
 
 	let {movement, buttons} = req.body;
 
-	let button = "mouseup 1";
+	let button = "mouseup 1 mouseup 2";
 	if(buttons.left_click){
 		button = "mousedown 1"
+	}
+	if(buttons.right_click){
+		button = "mousedown 2"
 	}
 
 	exec(`xdotool ${button} mousemove_relative -- ${movement.dx} ${movement.dy}`)	
@@ -72,10 +106,18 @@ app.post("/mouse", (req, res) => {
 
 app.post("/click", (req, res) => {
 
-	let {click} = req.body;
+	let { left_click, right_click } = req.body;
 
+	console.log(req.body);
 
-	exec(`xdotool click 1`)	
+	if(left_click){
+		exec(`xdotool click 1`)	
+	}		
+
+	if(right_click){
+		exec(`xdotool click 3`)	
+	}		
+
 
 	res.send("")
 
